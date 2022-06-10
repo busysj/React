699b87283130d199678e428962751352c5c0887e
SJ <tkdwox31__@naver.com>
2022-06-10 11:54:54 +0900

    0610 hook container

--- Redux/redux/src/containers/CounterHookContainer.jsx
+++ Redux/redux/src/containers/CounterHookContainer.jsx
@@ -17,11 +17,7 @@ const CounterHookContainer = () => {
   const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
   const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter
-      number={number}
-      onIncrease={() => dispatch(increase())}
-      onDecrease={() => dispatch(decrease())}
-    />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
