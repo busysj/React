d1a777b09009c9fcd65537367f4d601a92aef278
SJ <tkdwox31__@naver.com>
2022-07-04 11:26:35 +0900

    0704 pwa test

---
+++ PWA/react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>새로 만든 리액트</h1>
+        <p>서비스 워커를 추가하여 내용을 확인할 예정입니다</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
