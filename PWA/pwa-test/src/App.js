4298f6dcd427b42aee15fc09ee8559f21fed472a
SJ <tkdwox31__@naver.com>
2022-07-05 14:14:41 +0900

    0705 pwa test

---
+++ PWA/pwa-test/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>프로그래시브 웹앱</h1>
+        <p>위의 설치/이동 아이콘을 이용하여 웹 화면을 띄울 수 있다</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
